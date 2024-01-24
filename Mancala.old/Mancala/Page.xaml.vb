d20e414044519efb6bdd1cfe08c6f254558d2f06
Mark Nadig <mark@nadigs.net>
2024-01-24 16:50:41 -0700

    the old stuff

---
+++ Mancala.old/Mancala/Page.xaml.vb
@@ -0,0 +1,16 @@
+﻿Imports System
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Media
+Imports System.Windows.Media.Animation
+Imports System.Windows.Shapes
+
+Partial Public Class Page 
+	Inherits UserControl
+
+	Public Sub New()
+		' Required to initialize variables
+		InitializeComponent()
+	End Sub
+
+End Class
